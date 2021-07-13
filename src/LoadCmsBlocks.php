fd5cc511bbe9ac34fc0ddee44af6b15b7130c60c
alexander <coding@aggrosoft.de>
2021-07-13 19:12:04 +0200

    initial version

---
+++ src/LoadCmsBlocks.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Aggrosoft\LoadCmsBlocks;
+
+use Shopware\Core\Framework\Plugin;
+
+class LoadCmsBlocks extends Plugin
+{
+}
---
+++ src/Twig/LoadCmsBlockExtension.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace Aggrosoft\LoadCmsBlocks\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Shopware\Core\Content\Cms\SalesChannel\SalesChannelCmsPageLoader;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+
+use Twig\Environment;
+
+class LoadCmsBlockExtension extends AbstractExtension
+{
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    /**
+     * @var SalesChannelCmsPageLoader
+     */
+    private $cmsPageLoader;
+
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    public function __construct(Environment $twig, SalesChannelCmsPageLoader $cmsPageLoader, RequestStack $requestStack)
+    {
+        $this->twig = $twig;
+        $this->cmsPageLoader = $cmsPageLoader;
+        $this->requestStack = $requestStack;
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('cms', [$this, 'renderCms']),
+        ];
+    }
+
+    public function renderCms(string $id, SalesChannelContext $context)
+    {
+        $cmsPage = $this->cmsPageLoader->load($this->requestStack->getCurrentRequest(), new Criteria([$id]), $context)->first();
+        $result = $this->twig->render('@Storefront/storefront/page/content/detail.html.twig', ['cmsPage' => $cmsPage]);
+        return $result;
+    }
+}

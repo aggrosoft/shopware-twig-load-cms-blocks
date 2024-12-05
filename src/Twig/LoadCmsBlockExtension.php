d1b0a0025d0fb28aa1ebb7392dcab78731fdc602
alexanderkludt <coding@aggrosoft.de>
2024-12-05 10:34:31 +0100

    feat: use sub requests for rendering cms pages for better compatibility with other plugins (e.g. aggro-cms-page-extras)

--- src/Twig/LoadCmsBlockExtension.php
+++ src/Twig/LoadCmsBlockExtension.php
@@ -2,37 +2,26 @@
 
 namespace Aggrosoft\LoadCmsBlocks\Twig;
 
+use Shopware\Storefront\Controller\CmsController;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 use Shopware\Core\Content\Cms\SalesChannel\SalesChannelCmsPageLoaderInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 
 use Twig\Environment;
 
 class LoadCmsBlockExtension extends AbstractExtension
 {
-    /**
-     * @var Environment
-     */
-    private $twig;
-
-    /**
-     * @var SalesChannelCmsPageLoaderInterface
-     */
-    private $cmsPageLoader;
-
-    /**
-     * @var RequestStack
-     */
-    private $requestStack;
-
-    public function __construct(Environment $twig, SalesChannelCmsPageLoaderInterface $cmsPageLoader, RequestStack $requestStack)
+    public function __construct(
+        private readonly Environment $twig,
+        private readonly SalesChannelCmsPageLoaderInterface $cmsPageLoader,
+        private readonly RequestStack $requestStack,
+        private readonly HttpKernelInterface $kernel,
+    )
     {
-        $this->twig = $twig;
-        $this->cmsPageLoader = $cmsPageLoader;
-        $this->requestStack = $requestStack;
     }
 
     public function getFunctions()
@@ -44,8 +33,16 @@ class LoadCmsBlockExtension extends AbstractExtension
 
     public function renderCms(string $id, SalesChannelContext $context)
     {
-        $cmsPage = $this->cmsPageLoader->load($this->requestStack->getCurrentRequest(), new Criteria([$id]), $context)->first();
-        $result = $this->twig->render('@Storefront/storefront/page/content/detail.html.twig', ['cmsPage' => $cmsPage]);
-        return $result;
+        if ($this->requestStack->getParentRequest()){
+            // infinite loop protection
+            return '';
+        }
+
+        $request = $this->requestStack->getCurrentRequest();
+        $request->attributes->set('_route', 'frontend.cms.page');
+        $request->attributes->set('_controller', 'Shopware\Storefront\Controller\CmsController::page');
+        $request->attributes->set('id', $id);
+        $response = $this->kernel->handle($request, HttpKernelInterface::SUB_REQUEST);
+        return $response->getContent();
     }
 }
